293f28449899a94594567987f3768f936e895df0
Richard <richard760x@hotmail.com>
2022-01-07 22:30:36 -0500

    updated bio

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var i = 0;
 var t = 70;
-var str = "Hello, I am Richard, a Computer Science Grad! Take a look at my projects below!"
+var str = "Hello, I am Richard, I am a Computer Programmer and a Computer Science Grad! Take a look at my projects below!"
 
 function type() {
     if (i < str.length) {
